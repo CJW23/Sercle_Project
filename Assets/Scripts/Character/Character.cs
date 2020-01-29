fe4ffb4c5b89e9d31dc94f61fe802cbdc623244d
EQwon <bluetwintail0906@gmail.com>
2020-01-29 10:32:50 +0900

    Revert "전용 스킬 생성 중"
    This reverts commit df1c06b1e046c77f96e9308a0a692d63ab61b946.

--- Assets/Scripts/Character/Character.cs
+++ Assets/Scripts/Character/Character.cs
@@ -59,7 +59,6 @@ public class Character : MonoBehaviour
         foreach (Collider coll in colls)
         {
             if (coll.gameObject == gameObject) continue;
-            if (coll.gameObject.GetComponent<Character>().isFriend) continue;
 
             if (target == null || Vector3.Distance(coll.transform.position, transform.position) < nearestDis)
             {
